ef55d77328d22aa169e4020405b2a09d14d8b30c
Suchitra Naik <susanaik@gmail.com>
2018-07-12 20:13:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,6 +28,12 @@ class Route{
   }
 
 
-
+  estimatedTime(peak_hours){
+    if(peak_hours){
+        return this.blocksTravelled()/3
+    }else{
+      return this.blocksTravelled()/2
+    }
+  }
 
 }
