06407145f7c0e55bcefb0c32cde50188c9fa7e2a
Suchitra Naik <susanaik@gmail.com>
2018-07-12 19:38:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ class Route{
     this.endingLocation =  endingLocation
   }
   blocksTravelled(){
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
 
   }
 }
